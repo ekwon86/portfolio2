c96b50d8cdd9eb08ef9a4f497ca7e45d3ce9d2a1
Gene Kwon <ekwon86@gmail.com>
2017-10-10 21:31:37 -0700

    working on portfolio

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,6 +1,13 @@
 <template>
     <div id="Portfolio">
-        <div class="portfolio-content"></div>
+        <div class="portfolio-content">
+          <p class="headings portfolio-heading">Portfolio</p>
+          <div class="project-container">
+            <div class="indiv-project-subcontainers" v-for="project in lfzProjects" :style="{ 'background': 'url(' + project.imgPath + ')' }">
+              <h3>{{ project.name }}</h3>
+            </div>
+          </div>
+        </div>
     </div>
 </template>
 
@@ -8,6 +15,29 @@
     export default{
         data(){
             return {
+              lfzProjects: [
+                {
+                  name: 'Mega Match Man',
+                  url: 'http://www.eugenekwon.com/projects/megamatchman',
+                  imgPath: '../assets/megamatchman.png'
+                },
+                {
+                  name: 'Calculator',
+                  url: 'http://www.eugenekwon.com/projects/calculator',
+                  imgPath: '../assets/calculator.png'
+                },
+                {
+                  name: 'I Want',
+                  url: 'http://www.eugenekwon.com/projects/iwant',
+                  imgPath: '../assets/iwant.png'
+                }
+              ],
+              workProjects: [
+                {
+                  name: 'Conference 2017 Site',
+                  url: 'http://www.laserapp.com/conference2017'
+                }
+              ]
             }
         }
     }
@@ -15,10 +45,9 @@
 
 <style lang="scss">
     #Portfolio {
-        height: 1000px;
+        height: 1500px;
         position: relative;
         width: 100vw;
-        border: 1px solid red;
         background-color: white;
         padding: 5% 15%;
         .portfolio-content {
@@ -27,7 +56,29 @@
             width: 100%;
             transform:translateY(-50%);
             height: 100%;
-            border: 1px solid blue;
+            .portfolio-heading {
+              border-bottom: 1px solid rgba(0,0,0,0.1);
+            }
         }
+    }
+    .project-container {
+      min-height: 800px;
+      font-size: 0;
+      height: auto;
+      vertical-align: top;
+      border: 1px solid black;
+    }
+    .indiv-project-subcontainers {
+      border: 1px solid red;
+      padding: 25px;
+      width: 33.33%;
+      display: inline-block;
+      height: 250px;
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+      &:hover {
+        cursor: pointer;
+      }
     }
 </style>
